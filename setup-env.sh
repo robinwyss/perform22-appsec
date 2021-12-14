b3a60deca85ae2f72c942e20f94b8e0d678df375
alekseysirenko <68571134+alekseysirenko@users.noreply.github.com>
2021-12-14 14:32:57 -0500

    added comments

--- setup-env.sh
+++ setup-env.sh
@@ -25,26 +25,31 @@ fi
 #TODO how to install helm without prompt
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 
+echo "Installing Monaco"
 # install monaco
 curl -L https://github.com/dynatrace-oss/dynatrace-monitoring-as-code/releases/download/v1.6.0/monaco-linux-amd64 -o monaco 
 chmod +x monaco
 mv monaco /usr/local/bin/
 
+echo "Installing Keptn"
 # Install keptn
 curl -sL https://get.keptn.sh | sudo -E bash
-keptn install --endpoint-service-type=LoadBalancer --use-case=continuous-delivery
+yes | keptn install --endpoint-service-type=LoadBalancer --use-case=continuous-delivery
 KEPTN_ENDPOINT=http://$(kubectl get svc -n keptn api-gateway-nginx -ojsonpath='{.status.loadBalancer.ingress[0].hostname}')/api
 KEPTN_API_TOKEN=$(kubectl get secret keptn-api-token -n keptn -ojsonpath='{.data.keptn-api-token}' | base64 --decode)
 KEPTN_BRIDGE_URL=http://$(kubectl get svc -n keptn api-gateway-nginx -ojsonpath='{.status.loadBalancer.ingress[0].hostname}')/bridge
 keptn auth --endpoint=$KEPTN_ENDPOINT --api-token=$KEPTN_API_TOKEN
 
+echo "Installing Istio"
 # Install Istio 
 curl -L https://istio.io/downloadIstio | ISTIO_VERSION=$KEPTN_DYNATRACE_SERVICE_VERSION TARGET_ARCH=x86_64 sh -
 istio-$KEPTN_DYNATRACE_SERVICE_VERSION/bin/istioctl install
 
+echo "Installing Dynatrace Operator"
 # Install Dynatrace Operator
 wget https://github.com/dynatrace/dynatrace-operator/releases/latest/download/install.sh -O install.sh && sh ./install.sh --api-url "${DT_TENANT}/api" --api-token $DT_API_TOKEN --paas-token $DT_PAAS_TOKEN --cluster-name "keptn-appsec"
 
+echo "Configure Dynatrace Service in Keptn"
 # Configure Dynatrace in keptn
 keptn create secret dynatrace --scope=dynatrace-service --from-literal="DT_TENANT=$DT_TENANT" --from-literal="DT_API_TOKEN=$DT_API_TOKEN"
 helm upgrade --install dynatrace-service\
@@ -56,20 +61,21 @@ helm upgrade --install dynatrace-service\
  --set dynatraceService.config.generateDashboards=true\
  --set dynatraceService.config.generateMetricEvents=true
 
+echo "Setup Gitea"
 # setup gitea helm yaml
 sed -e 's~domain.placeholder~'"$GIT_DOMAIN"'~' \
     -e 's~GIT_USER.placeholder~'"$GIT_USER"'~' \
     -e 's~GIT_PASSWORD.placeholder~'"$GIT_PASSWORD"'~' \
     ./gitea/helm/gitea-values.yaml > gitea-values-gen.yaml
 
+echo "Install Gitea"
  # Install gitea
 kubectl create namespace gitea
 helm repo add gitea-charts https://dl.gitea.io/charts/
 helm install gitea gitea-charts/gitea -f ./gitea/helm/gitea-values-gen.yaml --namespace gitea
 GIT_URL=$(kubectl get svc --namespace gitea gitea-http -ojsonpath='{.status.loadBalancer.ingress[0].hostname}')
 
-
-
+echo "Get Gitea token"
 GIT_TOKEN=$(curl -v --user labuser:!Perform2022@ \
     -X POST "$GIT_URL/api/v1/users/labuser/tokens" \
     -H "accept: application/json" -H "Content-Type: application/json; charset=utf-8" \
@@ -79,13 +85,19 @@ curl -X POST "$GIT_URL/api/v1/user/repos" \
     -H "Authorization:token $GIT_TOKEN" \
     -d "{ \"auto_init\": false, \"default_branch\": \"main\", \"name\": \"$GIT_REPO\", \"private\": false}"
 
+echo "Create Keptn Project"
 # create keptn project
 keptn create project $KEPTN_PROJECT_NAME  --shipyard=./shipyard.yaml --git-user=$GIT_USER --git-token=$GIT_TOKEN --git-remote-url=http://$GIT_URL/$GIT_USER/$GIT_REPO.git
+
+echo "Create simplenode Service"
 keptn create service simplenode --project=$KEPTN_PROJECT_NAME
 # add helm chart for service
+echo "Add simplenode charts"
 keptn add-resource --project=$KEPTN_PROJECT_NAME --service=simplenode --resource=./keptn/simplenode.tgz --resourceUri=helm/simplenode.tgz --all-stages
 # add jmx config
+echo "Add JMeter test"
 keptn add-resource --project=$KEPTN_PROJECT_NAME --stage=staging --service=simplenode --resource=./keptn/jmeter/load.jmx --resourceUri=jmeter/load.jmx
 # deploy initial version
+echo "Deploy simplenode v1"
 keptn trigger delivery --project=$KEPTN_PROJECT_NAME --service=simplenode  --image=docker.io/robinwyss/simplenodeservice --tag=1.0.1
 
